823401043a9d0dee13a2a9c4b9c3f1c4813ca515
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-05-09 17:57:57 -0700

    added tests

--- fysan_tests.R
+++ fysan_tests.R
@@ -15,4 +15,36 @@ fyleSender("hahlas@hotmail.com",
            fyleIdentifier(fyle_location = test_directory, fyle_extension = "txt", fyle_exclusions = "hi0"),
            "test emailxxx", "here is the body")
 
+# Remove directories
+unlink(test_directory, recursive = TRUE, force = TRUE)
 
+
+# Test 2 ------------------------------------------------------------------
+
+
+test_directory <- "C:/Users/hahla/Desktop/Toss/fysan_tests"
+dir.create(test_directory)
+
+sampleDirectoryBuilder <- function(new_directory_tag) {#print(new_directory_tag)}
+  new_directory_name = paste0(test_directory, "/test_directory_", new_directory_tag)
+  dir.create(new_directory_name)
+  writeLines(paste("Sample text", new_directory_tag), paste0(new_directory_name, "/text_file_", new_directory_tag, ".txt"))
+  writeLines(paste("Sample script", new_directory_tag), paste0(new_directory_name, "/script_file_", new_directory_tag, ".R"))
+  writeLines(paste("ksfjew989j", new_directory_tag), paste0(new_directory_name, "/image_", new_directory_tag, ".jpg"))
+  dir.create(paste0(new_directory_name, "/logs"))
+  writeLines(paste("Log for ", new_directory_tag), paste0(new_directory_name, "/logs/logs_", new_directory_tag, ".txt"))
+}
+
+# Directory names
+sample_directory_list <- c("blue", "gold", "red", "white")
+
+# Build directories
+lapply(sample_directory_list, sampleDirectoryBuilder)
+
+# Mail files
+fyleSender("hahlas@hotmail.com",
+           fyleIdentifier(fyle_location = test_directory, fyle_extension = "jpg", fyle_exclusions = "red"),
+           "test emailxxx", "here is the body")
+
+# Remove directories
+unlink(test_directory, recursive = TRUE, force = TRUE)
