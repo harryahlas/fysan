8645d9f83c54acc5157569c0a7b52db8073d5001
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-05-09 23:10:22 -0700

    1st build

--- R/fysan.R
+++ R/fysan.R
@@ -4,7 +4,7 @@
 #' @param batch How many attachments per email. Defaults to 3.
 #' @param interval How many seconds to wait between each email send. Defaults to 1.  Can be handy to keep from overloading.
 #' @param fyle_location Location of directory to search. Will default to search recursively. Required.
-#' @param fyle_extension The extension(s) you want to search for, e.g. <code>c("txt", "R")</code> will send files that end in .txt or .R
+#' @param fyle_extension The extension(s) you want to search for, e.g. *c("txt", "R")* will send files that end in .txt or .R
 #' @param fyle_exclusions Files with this string anywhere in the file name or location will not be sent.  Optional.
 #' @param list_recursive Set to FALSE if you do not want to search through children of *fyle_location*. Generally keep TRUE.
 #' @param list_full_names List the full names of the location. Generally keep TRUE
