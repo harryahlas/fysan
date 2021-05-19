4bb48403a54f726bf087f2dd961ee99476eb4dd8
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-05-18 21:30:11 -0700

    quasi code

--- R/fyleGluer.R
+++ R/fyleGluer.R
@@ -1,1 +1,19 @@
-fyleGluer <- function() {}
+fyleGluer <- function(temp_folder = "saved_attachment_fyles") {
+
+  list.files(temp_folder)
+
+  # identify broken files into data frame
+
+  # identify the name of what the glued file will be
+
+  # For each broken file
+
+    # put the splits in order
+
+    # Identify how many splits there were
+
+    # for each split (minus 1) get the contents of the attachment
+
+    # write/append the contents of the attachment
+
+}
